8032eeaeb9e916a672e471d549dcd1d1ac569595
erik-marsh <erik.i.marsh@gmail.com>
2020-02-20 20:50:59 -0800

    Made poll() return a list of messages that can be sent via Discord

--- api.py
+++ api.py
@@ -5,9 +5,12 @@ import google_auth_oauthlib
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+# utility functions
+
+
 
 # TODO: Make a proper interface using metaclasses
-class AbstrctAPIInterface:
+class AbstractAPIInterface:
     def poll(self):
         pass
     
@@ -15,7 +18,7 @@ class AbstrctAPIInterface:
         pass
 
 
-class YouTubeAPI(AbstrctAPIInterface):
+class YouTubeAPI(AbstractAPIInterface):
     def __init__(self):
         # constants
         self.YOUTUBE_API_SERVICE_NAME = "youtube"
@@ -30,8 +33,14 @@ class YouTubeAPI(AbstrctAPIInterface):
         # build youtube api
         self.api = build(self.YOUTUBE_API_SERVICE_NAME, self.YOUTUBE_API_VERSION, developerKey=self.key)
 
+        #should we store this info in program memory or a file?
+        self.currResults = []
+        self.prevResults = []
+
     def poll(self):
-        result = []
+        #if len(self.currResults) == 0
+        self.prevResults = self.currResults
+        self.currResults = []
 
         for ch in self.channels:
             request = self.api.activities().list(
@@ -49,16 +58,36 @@ class YouTubeAPI(AbstrctAPIInterface):
             videoID = videoID.strip("\"") #videoID initially has a pair of quotes surrounding it
 
             link = "https://www.youtube.com/watch?v=" + videoID
-            result.append(dict([("title", title), ("link", link)])) #adds a new entry to the result
+            self.currResults.append(dict([("title", title), ("link", link)])) #adds a new entry to the result
 
-        return result
+        # check for changed results, add them to a new list,
+        # create a list of messages, and return the messages
+        newResults = []
+        for d in self.currResults:
+            if d in self.prevResults: #also try "not in"
+                continue
+            newResults.append(d)
         
-    def createMessage()
+        return self.createMessages(newResults)
 
+    # in a perfect world this would be a private method,
+    # (ie: don't call this from outside the class)
+    # in an even better world this would be a global function b/c abstraction is nice
+    def createMessages(self, results):
+        if len(results) == 0:
+            print("YOUTUBE: no messages to create")
+            return []
+
+        msgs = []
+        for r in results:
+            msg = r["title"] + "\n" + r["link"]
+            msgs.append(msg)
+        return msgs
+
+### main for testing
 yt = YouTubeAPI()
-temp = yt.poll()
 
-for d in temp:
-    print(d["title"])
-    print(d["link"])
+for i in range(2):
+    for m in yt.poll():
+        print(m)
     print()
